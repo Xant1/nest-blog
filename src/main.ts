a7bdbe3ec7e59f11a5facd7ce100210cbd5cbb61
xant1 <qazaqgun@gmail.com>
2023-01-24 22:51:59 +0500

    ejs

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { PORT } from './config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views/pages'));
   app.setViewEngine('ejs');
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,31 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Redirect, Render, Res } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostService } from './post.service';
-
+import { Response } from 'express';
 
 @Controller()
 export class PostController {
   constructor(private postService: PostService) {}
 
-  
+  @Get('/create')
+  root(@Res() res: Response) {
+    return res.render('create')
+  }
 
   @Post('/create')
+  @Redirect('/')
   createPost(@Body() dto: CreatePostDto) {
     return this.postService.create(dto);
   }
 
   @Get('/')  
-  getAllPosts() {
-    return this.postService.findAll();
+  getAllPosts( @Res() res: Response) {
+    this.postService.findAll()
+    .then((data) => {
+      res.render('index', {article: data})
+    })
   }
 
-  @Get(':id')
-  getOnePost(@Param('id') id: number) {
-    return this.postService.findOne(id);
-  }
+  // @Get(':id')
+  // getOnePost(@Param('id') id: number) {
+  //   return this.postService.findOne(id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.postService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: number) {
+  //   return this.postService.remove(id);
+  // }
 }
